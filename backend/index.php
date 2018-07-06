8617b2dfbf63727b08c25ca169d0a80e016843dc
Luisanc <luisanc.lanc@gmail.com>
2018-07-05 21:51:14 -0500

    Nueva estructura
    Estructura de proyecto con codeigniter

--- backend/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-/**
- * Created by Lanc.
- * Date: 06/06/2018
- */
---
+++ backend/upqroo/application/core/m_controller.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ *
+ */
+class ClassName extends Ci_Controller
+{
+
+  //Revisa el estado del usuario (logueado=true, noLogueado=false)
+  public function checkLogin()
+  {
+    //return status
+  }
+
+  //Envia peticion al web service, esperando una respuesta,
+  //si la respuesta es positiva llena la variable de sesion
+  public function login($user,$pass)
+  {
+    //return status
+  }
+
+  //Limpia las variables de sesion
+  public function logout()
+  {
+    //return status
+  }
+
+  //Envia un post al webservice con la informacion necesaria para crear una enviarNotificacion
+  //titulo
+  //descripcion
+  public function enviarNotificacion()
+  {
+    //return success
+  }
+
+
+  //Carga las vistas publicas
+  public function loadView($view)
+  {
+      //$this->load->view('templates/header');
+      $this->load->view($view);
+      //$this->load->view('template/footer');
+  }
+
+
+  //Carga la vista de administrador
+  public function loadViewAdmin($view)
+  {
+
+  }
+}
+?>
