87781e671e61e2525396e677ffd96906e1b5ec46
DESKTOP-P9GCB9N\Admin <rubi_030198@hotmail.com>
2018-07-11 12:21:07 -0500

    login view
    Cambié la vista login de html a php.

---
+++ backend/upqroo/application/views/login.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="../assets/css/trejocode.css" media="screen">
+        <link rel="stylesheet" href="../assets/css/login.css" media="screen">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+        <title>UPQROO - LOGIN</title>
+    </head>
+    <body>
+        <div class="login flex">
+            <div class="row-responsive">
+                <div class="left"></div>
+                <div class="right">
+                    <div class="justify-center align-center">
+                        <div class="container">
+                            <div class="column">
+                                <div class="logo">
+                                    <div class="responsive-img justify-center">
+                                        <img src="../assets/img/logo-aguila.png" alt="">
+                                    </div>
+                                </div>
+                                <div class="white-space-32"></div>
+                                <h2 class="text-center">ADMINISTRADOR WEB</h2>
+                                <div class="white-space-32"></div>
+                                <div class="login-form justify-center">
+                                    <div class="container">
+                                        <form class="" method="post">
+                                            <div class="column">
+                                                <label for="email">CORREO:</label>
+                                                <div class="white-space-8"></div>
+                                                <input class="input-text" type="email" name="email">
+                                                <div class="white-space-16"></div>
+                                                <label for="">CONTRASEÑA:</label>
+                                                <div class="white-space-8"></div>
+                                                <input class="input-text" type="password" name="password">
+                                                <div class="white-space-32"></div>
+                                                <input class="btn btn-login" type="submit" value="INGRESAR">
+                                                <div class="white-space-32"></div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
