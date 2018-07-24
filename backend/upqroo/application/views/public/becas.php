95133762da72fd1c11014add2e55a04268ca202d
DESKTOP-NN0IMN3\Admin <edwardchipat@gmail.com>
2018-07-24 11:52:21 -0500

    Vistas beca, nosotros y pagos.
    Archivos html a php.

---
+++ backend/upqroo/application/views/public/becas.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../assets/css/trejocode.css" media="screen">
+    <link rel="stylesheet" href="../assets/css/nosotros.css" media="screen">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+    <title>UPQROO</title>
+</head>
+
+    <div class="institutional flex">
+        <div class="column">
+            <!-- Banner -->
+            <div class="banner justify-center align-center" style="background-image: url(../assets/img/header-nosotros.jpg);">
+                <div class="container">
+                    <div class="title">
+                        <h2>BECAS</h2>
+                    </div>
+                </div>
+            </div> <!-- End Banner -->
+            <div class="nosotros row justify-center">
+                <div class="container">
+                    <div class="row-responsive">
+                        <div class="mision">
+                            <div class="column">
+                                <div class="white-space-48"></div>
+                                
+                                
+                                
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+
+            </div>
+            <!-- Reglamento -->
+            <div class="ubicacion justify-center">
+                <div class="container">
+                    <div class="column">
+                        <h2>BECA DE MANUTENCIÓN 2017 - 2018</h2>
+                        <div class="white-space-32"></div>
+                        
+                        <a class="link" href="http://www.upqroo.edu.mx/wp-content/uploads/2016/11/manual_general%20SUBES.pdf"><i class="fas fa-book"></i> SUBES MANUAL DE REGISTRO</a>
+                        <div class="white-space-16"></div>
+                        <a class="link" href="http://www.upqroo.edu.mx/wp-content/uploads/2017/09/cambio_contrasena.pdf"><i class="fas fa-book"></i> CAMBIO DE CONTRASEÑA</a>
+                        <div class="white-space-16"></div>
+                        <a class="link" href="http://www.upqroo.edu.mx/wp-content/uploads/2017/09/cambio_correo.pdf"><i class="fas fa-book"></i> CAMBIO DE CORREO</a>
+                        <div class="white-space-16"></div>
+                        <a class="link" href="http://www.upqroo.edu.mx/wp-content/uploads/2017/09/informacion_domicilio.pdf"><i class="fas fa-book"></i> INFORMACIÓN DE DOMICILIO</a>
+                        <div class="white-space-16"></div>
+                        <a class="link" href="http://www.upqroo.edu.mx/wp-content/uploads/2017/09/informacion_escolar.pdf"><i class="fas fa-book"></i> INFORMACIÓN ESCOLAR</a>
+                        <div class="white-space-16"></div>
+                        <a class="link" href="http://www.upqroo.edu.mx/wp-content/uploads/2017/09/informacion_personal.pdf"><i class="fas fa-book"></i> INFORMACIÓN PERSONAL</a>
+                        <div class="white-space-16"></div>
+                        <a class="link" href="http://www.upqroo.edu.mx/wp-content/uploads/2018/02/Programa_Nacional_de_Becas_2018.pdf"><i class="fas fa-book"></i> CONVOCATORIA-BECA-MANUTENCIÓN-2017-2018</a>
+                        <div class="white-space-16"></div>
+                        <a class="link" href="http://www.upqroo.edu.mx/wp-content/uploads/2018/02/PUNTOS-IMPORTANTES-A-CONSIDERAR-CICLO-2017-2018EXTRAORDINARIA.pdf"><i class="fas fa-book"></i> PUNTOS IMPORTANTES PARA EL REGISTRO</a>
+                        <div class="white-space-16"></div>
+                        <P> LINKS PARA REALIZAR LA SOLICITUD</P>
+                        <div class="white-space-16"></div>
+                        <a class="link" href="https://www.subes.sep.gob.mx"><i class="fas fa-book"></i> https://www.subes.sep.gob.mx</a>
+                        <div class="white-space-16"></div>
+                        <a class="link" href="http://manutencion.seyc.gob.mx"><i class="fas fa-book"></i> http://manutencion.seyc.gob.mx</a>
+                        <div class="white-space-48"></div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Ubicación -->
+            <div class="ubicacion justify-center">
+                <div class="container">
+                    <div class="column">
+                        <h2>UBICACIÓN</h2>
+                        <div class="white-space-32"></div>
+                    </div>
+                </div>
+            </div>
+            <div style="width: 100%"><iframe width="100%" height="500" src="https://maps.google.com/maps?width=100%&height=600&hl=es&q=Av.%20Arco%20Bincentenario%2C%20Mza.%2011%2C%20Lote%201119-33%2C%20Sm%20255%2C%2077500%20Canc%C3%BAn%2C%20Q.R.+(Universidad%20Polit%C3%A9cnica%20de%20Quintana%20Roo)&ie=UTF8&t=&z=16&iwloc=B&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div><br />
+        </div> <!-- End Main Column -->
+    </div> <!-- End Nosotros Column -->
+
+
+
+<body>
+</body>
+</html>
---
+++ backend/upqroo/application/views/public/pagos.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../assets/css/trejocode.css" media="screen">
+    <link rel="stylesheet" href="../assets/css/pagos.css" media="screen">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+    <title>UPQROO</title>
+</head>
+
+    <div class="institutional flex">
+        <div class="column">
+            <!-- Banner -->
+            <div class="banner justify-center align-center" style="background-image: url(../assets/img/header-pagos.jpg);">
+                <div class="container">
+                    <div class="title">
+                        <h2>PAGOS Y CUOTAS</h2>
+                    </div>
+                </div>
+            </div> <!-- End Banner -->
+
+            <div class="directorio-otros justify-center">
+                <div class="container">
+                    <div class="column">
+                        <div class="white-space-64"></div>
+                        <div class="directorio-list">
+                            <div class="row-responsive justify-between">
+                                <div class="column auto">
+                                    <h3 class="color-teal">01 - EXAMEN DE ADMISIÓN </h3>
+                                    <div class="white-space-16"></div>
+                                    <h3 class="color-teal">02 - INSCRIPCIÓN NUEVO INGRESO 2018 Y POSTERIOR </h3>
+                                    <div class="white-space-16"></div>
+                                    <h3 class="color-teal">03 - REINSCRIPCIÓN GENERACIÓN ANTERIOR 2017 </h3>
+                                    <div class="white-space-16"></div>
+                                    <h3 class="color-teal">04 - REINSCRIPCIÓN GENERACIÓN 2016 Y 2017 </h3>
+                                    <div class="white-space-16"></div>
+                                    <h3 class="color-teal">05 - REGÍSTRO DE TÍTULO Y EXPEDICIÓN DE CÉDULA Y TÍTULO </h3>
+                                    <div class="white-space-16"></div>
+                                    <h3 class="color-teal">06 - ALTAS Y BAJAS POR MATERIA </h3>
+                                    <div class="white-space-16"></div>
+                                    <h3 class="color-teal">07 - CONSTANCIA DE COMPETENCIAS </h3>
+                                    <div class="white-space-16"></div>
+                                    <h3 class="color-teal">08 - CONSTANCIA DE ESTUDIOS </h3>
+                                    <div class="white-space-16"></div>
+                                    <h3 class="color-teal">09 - EXPEDICIÓN DE CERTIFICADOS </h3>
+                                    <div class="white-space-16"></div>
+                                    <h3 class="color-teal">10 - REPOSICIÓN DE CREDENCIAL DE ESTUDIANTE </h3>
+                                    <div class="white-space-16"></div>
+                                    <h3 class="color-teal">11 - CAMBIO DE CARRERA </h3>
+                                    <div class="white-space-16"></div>
+                                    <h3 class="color-teal">12 - SOLICITUD DE REVALIDACIÓN O EQUIVALENCIA DE ESTUDIOS </h3>
+                                    <div class="white-space-16"></div>
+                                    <h3 class="color-teal">13 - INSCRIPCIÓN ESTUDIANTE EXTRANJERO </h3>
+                                    <div class="white-space-16"></div>
+                                    <h3 class="color-teal">14 - REINSCRIPCIÓN ESTUDIANTE EXTRANJERO </h3>
+                                    <div class="white-space-16"></div>
+                                    <h3 class="color-teal">15 - REPOSICIÓN DE BOLETA DE CALIFICACIONES </h3>
+                                    <div class="white-space-16"></div>
+                                    <h3 class="color-teal">16 - CURSO PROPEDEUTICO </h3>
+                                    <div class="white-space-16"></div>
+                                    
+                                </div>
+                                <div class="column auto">
+                                    <p class="color-teal"> $ 700</p>
+                                    <div class="white-space-16"></div>
+                                    <p class="color-teal"> $ 2,200</p>
+                                    <div class="white-space-16"></div>
+                                    <p class="color-teal"> $ 1,600</p>
+                                    <div class="white-space-16"></div>
+                                    <p class="color-teal"> $ 2,000</p>
+                                    <div class="white-space-16"></div>
+                                    <p class="color-teal"> $ 3,300</p>
+                                    <div class="white-space-16"></div>
+                                    <p class="color-teal"> $ 110</p>
+                                    <div class="white-space-16"></div>
+                                    <p class="color-teal"> $ 160</p>
+                                    <div class="white-space-16"></div>
+                                    <p class="color-teal"> $ 110</p>
+                                    <div class="white-space-16"></div>
+                                    <p class="color-teal"> $ 550</p>
+                                    <div class="white-space-16"></div>
+                                    <p class="color-teal"> $ 110</p>
+                                    <div class="white-space-16"></div>
+                                    <p class="color-teal"> $ 550</p>
+                                    <div class="white-space-16"></div>
+                                    <p class="color-teal"> $ 800</p>
+                                    <div class="white-space-16"></div>
+                                    <p class="color-teal"> $ 8,800</p>
+                                    <div class="white-space-16"></div>
+                                    <p class="color-teal"> $ 8,800</p>
+                                    <div class="white-space-16"></div>
+                                    <p class="color-teal"> $ 55</p>
+                                    <div class="white-space-16"></div>
+                                    <p class="color-teal"> $ 600</p>
+                                    <div class="white-space-16"></div>
+                                </div>
+                            </div>
+                        </div>      
+                        
+                    <div class="white-space-64"></div>
+                </div> <!-- End Main Column -->
+            </div> <!-- End Nosotros Column -->
+        </div>
+     </div>
+</div>
+
+
+
+<body>
+</body>
+</html>
