daf649b115a66bc774b3cad573ddd8240b37339c
AlejandroTrejoCode <alejandrotrejocode@gmail.com>
2018-07-25 22:59:13 -0500

    Noticas SEO

--- backend/upqroo/application/views/public/noticia.php
+++ backend/upqroo/application/views/public/noticia.php
@@ -22,16 +22,16 @@
                                         <i class="fas fa-tags"></i>
                                     </div>
                                     <div class="notice-tags align-center">
-                                        <a href="#">CONVENIOS</a>
-                                        <a href="#">FIRMAS</a>
-                                        <a href="#">OPORTUNIDADES</a>
+                                        <a href="#">NOTICIAS</a>
+                                        <a href="#">ESTUDIANTES</a>
+                                        <a href="#">COMUNICADOS</a>
                                     </div>
                                 </div>
                             </div>
                             <div class="white-space-8"></div>
                                 <div class="notice-banner">
                                     <div class="responsive-img">
-                                        <img src="<?php echo $portada; ?>" alt="">
+                                        <img src="<?php echo $portada; ?>" alt=" Imagen Destacada " title=" Imagen Destacada ">
                                     </div>
                                 </div>
                                 <div class="white-space-16"></div>
@@ -42,31 +42,31 @@
                                 </div>
                                 <div class="white-space-8"></div>
                                 <div class="notice-description">
-                                    
+
                                 </div>
                                 <div class="white-space-16"></div>
                                 <div class="notice-gallery">
                                 <div class="column">
                                     <div class="row-responsive">
                                         <div class="responsive-img">
-                                            <img src="../assets/img/noticia.jpg" alt="">
+                                            <img src="../assets/img/noticia.jpg" alt=" Imagen Galería " title=" Imagen Galería ">
                                         </div>
                                         <div class="responsive-img">
-                                            <img src="../assets/img/noticia.jpg" alt="">
+                                            <img src="../assets/img/noticia.jpg" alt=" Imagen Galería " title=" Imagen Galería ">
                                         </div>
                                         <div class="responsive-img">
-                                            <img src="../assets/img/noticia.jpg" alt="">
+                                            <img src="../assets/img/noticia.jpg" alt=" Imagen Galería " title=" Imagen Galería ">
                                         </div>
                                     </div>
                                     <div class="row-responsive">
                                         <div class="responsive-img">
-                                            <img src="../assets/img/noticia.jpg" alt="">
+                                            <img src="../assets/img/noticia.jpg" alt=" Imagen Galería " title=" Imagen Galería ">
                                         </div>
                                         <div class="responsive-img">
-                                            <img src="../assets/img/noticia.jpg" alt="">
+                                            <img src="../assets/img/noticia.jpg" alt=" Imagen Galería " title=" Imagen Galería ">
                                         </div>
                                         <div class="responsive-img">
-                                            <img src="../assets/img/noticia.jpg" alt="">
+                                            <img src="../assets/img/noticia.jpg" alt=" Imagen Galería " title=" Imagen Galería ">
                                         </div>
                                     </div>
                                 </div>
@@ -115,7 +115,7 @@
                                         <div class="column">
 
                                             <div class="thumb responsive-img">
-                                                <img src="<?php echo $Ultima1->portada; ?>" alt="">
+                                                <img src="<?php echo $Ultima1->portada; ?>" alt=" Imagen Destacada " title=" Imagen Destacada ">
                                             </div>
                                             <div class="white-space-8"></div>
                                             <div class="title-link">
@@ -125,7 +125,7 @@
                                             <div class="white-space-16"></div>
 
                                             <div class="thumb responsive-img">
-                                                <img src="<?php echo $Ultima2->portada; ?>" alt="">
+                                                <img src="<?php echo $Ultima2->portada; ?>" alt=" Imagen Destacada " title=" Imagen Destacada ">
                                             </div>
                                             <div class="white-space-8"></div>
                                             <div class="title-link">
@@ -135,7 +135,7 @@
                                             <div class="white-space-16"></div>
 
                                             <div class="thumb responsive-img">
-                                                <img src="<?php echo $Ultima3->portada; ?>" alt="">
+                                                <img src="<?php echo $Ultima3->portada; ?>" alt=" Imagen Destacada " title=" Imagen Destacada ">
                                             </div>
                                             <div class="white-space-8"></div>
                                             <div class="title-link">
