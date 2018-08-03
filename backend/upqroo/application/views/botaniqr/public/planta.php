bbb89d5f84d7f07e80d12839dc332d2bbadf2640
Luisanc <luisanc.lanc@gmail.com>
2018-08-02 22:03:01 -0500

    vistas de noticia y planta actualizadas
    Se adapto las vistas al estilo del css

--- backend/upqroo/application/views/botaniqr/public/planta.php
+++ backend/upqroo/application/views/botaniqr/public/planta.php
@@ -1,56 +1,68 @@
-<div class="white-space-25"></div>
-<div class="planta flex justify">
-    <div class="container">
-        <div class="column">
-            <div class="title">
-                <h2><?php echo $nombre;?></h2>
-            </div>
-            <div class="cname">
-                <h3><?php echo $cientifico;?></h3>
-            </div>
-            <div class="white-space-8"></div>
-            <div class="row-responsive">
-                <div class="plant-container">
-                    <div class="left">
-                        <div class="responsive-img" id="planta-img">
-                            <img src="<?php echo base_url().'BotaniQR/'.$imgUrl;?>" alt="Planta">
-                        </div>
+<div class="planta flex">
+    <div class="column">
+        <div class="white-space-32"></div>
+        <div class="row justify-center">
+            <div class="container">
+                <div class="column">
+                    <div class="title">
+                        <h2><?php echo $nombre; ?></h2>
                     </div>
-                    <div class="right justify">
+                    <div class="white-space-8"></div>
+                    <div class="cname">
+                        <h3><?php echo $cientifico; ?></h3>
+                    </div>
+                    <div class="white-space-32"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="justify-center">
+            <div class="container row-responsive">
+                <div class="imagen-planta">
+                    <div class="responsive-img" id="planta-img">
+                        <img src="<?php echo base_url().'BotaniQR/'.$imgUrl; ?>" alt="Planta">
+                    </div>
+                </div>
+                <div class="informacion-planta">
+                    <div class="column align-center">
                         <div class="container">
                             <div class="column">
-                                <div class="plant-description">
+                                <div class="descripcion-planta">
                                     <h3>DESCRIPCIÓN</h3>
                                 </div>
                                 <div class="white-space-8"></div>
-                                <div class="plant-description">
-                                    <p><?php echo $descripcion; ?></p>
+                                <div class="descripcion-planta">
+                                    <p>
+                                        <?php echo $descripcion; ?>
+                                    </p>
                                 </div>
-
-                                <div class="white-space-16"></div>
-                                <div class="plant-description">
+                                <div class="white-space-24"></div>
+                                <div class="descripcion-planta">
                                     <h3>TAXONOMÍA</h3>
                                 </div>
                                 <div class="white-space-8"></div>
-                                <div class="plant-description">
-                                    <p><?php echo $taxonomia;?></p>
+                                <div class="descripcion-planta">
+                                    <p>
+                                        <?php echo $taxonomia; ?>
+                                    </p>
                                 </div>
-
-                                <div class="white-space-16"></div>
-                                <div class="plant-description">
+                                <div class="white-space-24"></div>
+                                <div class="descripcion-planta">
                                     <h3>APLICACIONES</h3>
                                 </div>
                                 <div class="white-space-8"></div>
-                                <div class="plant-description">
-                                    <p><?php echo $aplicaciones;?></p>
+                                <div class="descripcion-planta">
+                                    <p>
+                                        <?php echo $aplicaciones; ?>
+                                    </p>
                                 </div>
-
+                                <div class="white-space-32"></div>
                             </div>
                         </div>
+
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-<div class="white-space-25"></div>
