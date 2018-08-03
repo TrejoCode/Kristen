55abb76db4f89bad0f680cda99adbf0748da97a1
mtmg <mmgescuelaup@users.noreply.github.com>
2018-08-03 00:15:27 -0500

    Modifica la base para crear editar/crear Planta

--- backend/upqroo/application/views/botaniqr/private/admin-add-plant.php
+++ backend/upqroo/application/views/botaniqr/private/admin-add-plant.php
@@ -5,7 +5,7 @@
             <div class="column">
                 <div class="white-space-16"></div>
                 <div class="title column">
-                    <h2>NUEVA NOTICIA</h2>
+                    <h2>NUEVA PLANTA</h2>
 
                     <!-- Campos Ocultos -->
                     <input class="input-text" type="text" hidden name="publicacion" value="<?php echo !empty($noticias->idPublicaciones)?$noticias->idPublicaciones:'';?>">
@@ -19,53 +19,28 @@
                     <!-- Fin Campos ocultos -->
 
                     <div class="white-space-16"></div>
-                    <h4>TÍTULO</h4>
+                    <h4>NOMBRE</h4>
                     <div class="white-space-8"></div>
-                    <input class="input-text" type="text" name="titulo" value="<?php echo !empty($noticias)?$noticias->titulo:'';?>">
+                    <input class="input-text" type="text" name="titulo" value="<?php echo !empty($plantas)?$plantas[0]->nombre:'';?>">
                 </div>
                 <div class="white-space-16"></div>
                 <div class="description column">
-                    <h4>DESCRIPCIÓN CORTA</h4>
+                    <h4>NOMBRE CIENTÍFICO</h4>
                     <div class="white-space-8"></div>
-                    <textarea class="input-textarea" name="descripcion" rows="2"><?php echo !empty($noticias)?$noticias->descripcion:'';?></textarea>
+                    <textarea class="input-textarea" name="descripcion" rows="2"><?php echo !empty($plantas)?$plantas[0]->cientifico:'';?></textarea>
                 </div>
                 <div class="white-space-32"></div>
-                <h2>CONTENIDO</h2>
-                <div class="white-space-32"></div>
-                <h4>PARRAFO 1</h4>
+                <h4>DESCRIPCIÓN</h4>
                 <div class="white-space-8"></div>
-                <textarea class="input-textarea" name="p[]" rows="4"><?php echo !empty($noticias->contenidos[0]->contenido->texto)?$noticias->contenidos[0]->contenido->texto:'';?></textarea>
+                <textarea class="input-textarea" name="p[]" rows="4"><?php echo !empty($plantas)?$plantas[0]->descripcion:'';?></textarea>
                 <div class="white-space-16"></div>
-                <h4>PARRAFO 2</h4>
+                <h4>TAXONOMÍA</h4>
                 <div class="white-space-8"></div>
-                <textarea class="input-textarea" name="p[]" rows="4"><?php echo !empty($noticias->contenidos[1]->contenido->texto)?$noticias->contenidos[1]->contenido->texto:'';?></textarea>
+                <textarea class="input-textarea" name="p[]" rows="4"><?php echo !empty($plantas)?$plantas[0]->taxonomia:'';?></textarea>
                 <div class="white-space-16"></div>
-                <h4>PARRAFO 3</h4>
+                <h4>APLICACIONES</h4>
                 <div class="white-space-8"></div>
-                <textarea class="input-textarea" name="p[]" rows="4"><?php echo !empty($noticias->contenidos[2]->contenido->texto)?$noticias->contenidos[2]->contenido->texto:'';?></textarea>
-                <div class="white-space-16"></div>
-                <h4>GALERÍA</h4>
-                <p><em>SOLO PUEDEN SUBIR 6 IMÁGENES</em></p>
-                <div class="white-space-8"></div>
-                <div><a class="btn btn-blue" id="gallery-img-click" href="#"><i class="fas fa-file-upload"></i> SUBIR IMÁGENES</a></div>
-                <input class="input-file-h" id="gallery-img" type="file" name="gallery-img[]" multiple>
-                <div class="white-space-32"></div>
-                <h4>ENLACE 1</h4>
-                <div class="white-space-8"></div>
-                <input class="input-text" type="text" name="url-name[]" placeholder="Título" value="<?php echo !empty($noticias->contenidos[3]->contenido->texto)?$noticias->contenidos[3]->contenido->texto:'';?>">
-                <div class="white-space-8"></div>
-                <input class="input-text" type="text" name="url[]" value="<?php echo !empty($noticias->contenidos[3]->contenido->url)?$noticias->contenidos[3]->contenido->url:'';?>">
-                <h4>ENLACE 2</h4>
-                <div class="white-space-8"></div>
-                <input class="input-text" type="text" name="url-name[]" placeholder="Título" value="<?php echo !empty($noticias->contenidos[4]->contenido->texto)?$noticias->contenidos[4]->contenido->texto:'';?>">
-                <div class="white-space-8"></div>
-                <input class="input-text" type="text" name="url[]" value="<?php echo !empty($noticias->contenidos[4]->contenido->url)?$noticias->contenidos[4]->contenido->url:'';?>">
-                <div class="white-space-8"></div>
-                <h4>ENLACE 3</h4>
-                <div class="white-space-8"></div>
-                <input class="input-text" type="text" name="url-name[]" placeholder="Título" value="<?php echo !empty($noticias->contenidos[5]->contenido->texto)?$noticias->contenidos[5]->contenido->texto:'';?>">
-                <div class="white-space-8"></div>
-                <input class="input-text" type="text" name="url[]" value="<?php echo !empty($noticias->contenidos[5]->contenido->url)?$noticias->contenidos[5]->contenido->url:'';?>">
+                <textarea class="input-textarea" name="p[]" rows="4"><?php echo !empty($plantas)?$plantas[0]->aplicaciones:'';?></textarea>
                 <div class="white-space-16"></div>
             </div>
         </div>
@@ -75,27 +50,17 @@
             <div class="container">
                 <div class="column">
                     <div class="white-space-16"></div>
-                    <h4>IMAGEN DE PORTADA</h4>
+                    <h4>IMAGEN DE PLANTA</h4>
                     <div class="white-space-16"></div>
                     <div class="featured-img responsive-img">
                         <!-- Susituir por la ruta de la portada actual -->
-                        <img src="<?php echo !empty($noticias->portada)?base_url().$noticias->portada:''; ?>" alt="">
+                        <img src="<?php echo !empty($plantas[0]->imagen)?base_url().'BotaniQR/'.$plantas[0]->imagen:''; ?>" alt="">
                     </div>
                     <div class="white-space-8"></div>
-                    <div><a class="btn btn-blue" id="featured-img-click" href="#"><i class="fas fa-file-upload"></i> CAMBIAR IMAGEN</a></div>
+                    <div><a class="btn btn-blue" id="featured-img-click" href="#"><i class="fas fa-file-upload"></i> SUBIR IMAGEN</a></div>
                     <input class="input-file-h" id="featured-img" type="file" name="portada">
                     <div class="white-space-32"></div>
-                    <h4>ETIQUETAS</h4>
-                    <div class="white-space-8"></div>
-                    <p>Sepáralas por coma</p>
-                    <div class="white-space-16"></div>
-                        <input class="input-text" type="text" name="tags" value="<?php echo !empty($noticias)?$noticias->categorias:'';?>">
-                    <div class="white-space-16"></div>
-                    <div class="row">
-                        <label><input type="checkbox" name="notificacion"><span> ENVIAR NOTIFICACIÓN</span></label>
-                    </div>
-                    <div class="white-space-32"></div>
-                    <input class="btn btn-blue" type="submit" value="PUBLICAR">
+                    <input class="btn btn-blue" type="submit" value="AGREGAR">
                 </div>
             </div>
         </div>
