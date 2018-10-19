33b4c2891ad5321237461d8915acf31616a276dd
Sergio Trejo <alejandrotrejocode@gmail.com>
2018-10-18 23:46:37 -0500

    Visitas

---
+++ frontend/templates/visitas.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../assets/css/trejocode.css" media="screen">
+    <link rel="stylesheet" href="../assets/css/nosotros.css" media="screen">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+    <title>UPQROO</title>
+</head>
+
+    <div class="institutional flex">
+        <div class="column">
+            <div class="banner justify-center align-center" style="background-image: url(../assets/img/header-nosotros.jpg);">
+                <div class="container">                    
+                    <h2 class="color-white">
+                        VISITAS INDUSTRIALES
+                    </h2>
+                </div>
+            </div> <!-- End Banner -->
+
+            <div class="componente-miniheader justify-center">
+                <div class="container column">
+                    <div class="white-space-64"></div>
+                    <h2>
+                        Tu contacto con el sector productivo
+                    </h2>
+                    <div class="white-space-16"></div>
+                    <p>
+                        ¿Te gustaría conocer como funcionan las empresas y la industria?
+                    </p>
+                    <div class="white-space-8"></div>
+                    <p>
+                        Realiza los siguientes pasos:
+                    </p>
+                    <div class="white-space-64"></div>
+                </div>
+            </div>
+
+            <div class="white-space-64"></div>
+            
+            <div class="skills justify-center skills-visitas">
+                <div class="container">
+                    <div class="column">
+                        <div class="component-habilities">
+                            <div class="column">
+                                <div class="row-responsive">
+                                    <div class="hability">
+                                        <div class="hability-number auto">
+                                            <h2 class="color-lightBlue">
+                                                01
+                                            </h2>
+                                        </div>
+                                        <div class="hability-description column">
+                                            <h4>
+                                                Programar la Visita
+                                            </h4>
+                                            <p class="text-justify">
+                                                Dos semanas antes o dentro de los primeros diez hábiles de que comience cada cuatrimestre, las Coordinaciones de
+                                                Carrera deberán entregar al Departamento de Vinculación, la programación cuatrimestral de Visitas Industriales a través
+                                                del Formato Plan cuatrimestral de visitas.
+                                            </p>
+                                            <div class="white-space-24"></div>
+                                        </div>
+                                    </div>
+                                    <div class="hability">
+                                        <div class="hability-number auto">
+                                            <h2 class="color-lightBlue">
+                                                02
+                                            </h2>
+                                        </div>
+                                        <div class="hability-description column">
+                                            <h4>
+                                                Solicitar la Visita
+                                            </h4>
+                                            <p class="text-justify"> 
+                                                Una vez recibido el formato UPQ-F080 la Dirección de Vinculación procederá a establecer contacto con la(s) empresa(s)
+                                                que en particular se desea visitar o con aquellas que por su giro, características, facilidades y ubicación pueden ser
+                                                visitadas, siempre buscando satisfacer el objetivo que para tal efecto se plantea la visita.
+                                            </p>
+                                            <div class="white-space-24"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row-responsive">
+                                    <div class="hability">
+                                        <div class="hability-number auto">
+                                            <h2 class="color-lightBlue">
+                                                03
+                                            </h2>
+                                        </div>
+                                        <div class="hability-description column">
+                                            <h4>
+                                                Agendar la visita
+                                            </h4>
+                                            <p class="text-justify">
+                                                Recibida en la Dirección de Vinculación la respuesta por parte de la empresa, se procederá a notificar a la Dirección
+                                                de Programa Educativo, la fecha, lineamientos de seguridad y una copia del formato UPQ-F38, así como la solicitud de
+                                                que sea proporcionada una lista con nombres y número de seguro facultativo o seguro popular de cada asistente, dicha
+                                                lista deberá ser entregada a Vinculación con mínimo 5 días de anticipación al evento o en el término más amplio que
+                                                disponga la empresa a visitar.
+                                            </p>
+                                            <div class="white-space-24"></div>
+                                        </div>
+                                    </div>
+                                    <div class="hability">
+                                        <div class="hability-number auto">
+                                            <h2 class="color-lightBlue">
+                                                04
+                                            </h2>
+                                        </div>
+                                        <div class="hability-description column">
+                                            <h4>
+                                                Seguro facultativo
+                                            </h4>
+                                            <p class="text-justify">
+                                                La Dirección de Programa Educativo, recabará copia de comprobante de vigencia del Seguro Facultativo, e invitando a
+                                                aquel alumno que no esté dado de alta, lo haga a la brevedad. Ningún alumno o docente que no cuente con seguridad
+                                                social vigente podrá asistir a la Visita Industrial Programada.
+                                            </p>
+                                            <div class="white-space-24"></div>
+                                        </div>
+                                    </div>                                    
+                                </div>
+                                <div class="row-responsive">
+                                    <div class="hability">
+                                        <div class="hability-number auto">
+                                            <h2 class="color-lightBlue">
+                                                05
+                                            </h2>
+                                        </div>
+                                        <div class="hability-description column">
+                                            <h4>
+                                                Lista de asistentes
+                                            </h4>
+                                            <p class="text-justify">
+                                                La Dirección de Programa Educativo, con sustento a la información recabada y a la que se refiere el Paso 4 de este
+                                                tutorial, proporcionará a la Dirección de Vinculación la lista y número de seguro facultativo o popular de los
+                                                asistentes en los plazos que se indican en el Paso 3, y en caso de requerir la transportación institucional para llevar
+                                                a cabo dicha visita deberá ser indicado por escrito al momento de entregar la información antes mencionada.
+                                            </p>
+                                            <div class="white-space-24"></div>
+                                        </div>
+                                    </div>
+                                    <div class="hability">
+                                        <div class="hability-number auto">
+                                            <h2 class="color-lightBlue">
+                                                06
+                                            </h2>
+                                        </div>
+                                        <div class="hability-description column">
+                                            <h4>
+                                                Realizar visita
+                                            </h4>
+                                            <p class="text-justify">
+                                                Se realiza visita industrial en apego a las directrices marcadas por la empresa y el docente responsable del
+                                                acompañamiento de los estudiantes, llenará en ese momento, el formato UPQ-F38 de Reporte de Visita Industrial que le
+                                                debe de proporcionar la Dirección de Programa Educativo, procediendo a recabar además la firma y sello de la
+                                                representación de la empresa. Notas:
+                                            </p>
+                                            <div class="white-space-24"></div>
+                                        </div>
+                                    </div>                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> <!-- End Skill -->
+
+
+            <div class="directorio-otros justify-center">
+                <div class="container">
+                    <div class="column">
+                        <div class="white-space-64"></div>
+                        <div class="directorio-list">
+                            <div class="row-responsive justify-between">
+                                <div class="column auto">
+                                    <h3>
+                                        Importante
+                                    </h3>
+                                    <div class="white-space-16"></div>
+                                    <p class="text-justify">
+                                        El Reporte de Visita Industrial deberá ser entregado en forma inmediata a la Dirección de Vinculación, por parte de la Dirección de Programa Educativo.
+                                    </p>
+                                    <div class="white-space-8"></div>
+                                    <p class="text-justify">
+                                        En caso requerir servicios de transporte privado, los recursos económicos y contacto de proveedores deberán ser gestionados por la Dirección de Programa Educativo correspondiente.
+                                    </p>
+                                    <div class="white-space-8"></div>
+                                    <p class="text-justify">
+                                        Las visitas industriales <strong>no programadas</strong> y/o no notificadas a la Dirección de Vinculación, no recibirán el apoyo de transporte institucional.
+                                    </p>
+                                    <div class="white-space-32"></div>
+                                    <h3>
+                                        Descarga los formatos
+                                    </h3>
+                                    <div class="white-space-16"></div>
+                                    <p class="text-justify">
+                                        En cuanto a documentos de Referencia de la Dirección de Vinculación sobre esta actividad se desprende:
+                                    </p>
+                                    <div class="white-space-32"></div>
+                                    <div class="color-lightBlue">
+                                        <a class="link" href="#">
+                                            <i class="fas fa-book"></i> Formato de Reporte de Visita Industrial visible en Anexo 52 UPQ-F043.
+                                        </a>
+                                    </div>                                    
+                                    <div class="white-space-32"></div>
+                                    <div class="color-lightBlue">
+                                        <a class="link" href="#">
+                                            <i class="fas fa-book"></i> Formato del Plan Cuatrimestral de Visitas Industriales visible en Formato UPQ-F08
+                                        </a>
+                                    </div>                                    
+                                    <div class="white-space-64"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>        
+
+        </div>
+    </div>
+</html>
